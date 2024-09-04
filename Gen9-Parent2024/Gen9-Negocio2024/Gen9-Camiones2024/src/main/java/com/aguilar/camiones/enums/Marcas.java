4b759007e3f7a6683147bf52ae537d17a509a7fc
FOLDENZ\perre <jaguilar2019@gmail.com>
2024-09-04 13:54:32 -0600

    Se concluye el servicio de camiones

---
+++ Gen9-Parent2024/Gen9-Negocio2024/Gen9-Camiones2024/src/main/java/com/aguilar/camiones/enums/Marcas.java
@@ -0,0 +1,14 @@
+package com.aguilar.camiones.enums;
+
+public enum Marcas {
+    VOLVO,
+    ALLIANCE,
+    FORD,
+    MERCEDES_BENZ,
+    DINA;
+
+
+
+
+
+}
---
+++ Gen9-Parent2024/Gen9-Negocio2024/Gen9-Camiones2024/src/main/java/com/aguilar/camiones/services/CamionesService.java
@@ -0,0 +1,46 @@
+package com.aguilar.camiones.services;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.aguilar.camiones.dao.ICamionesDao;
+import com.aguilar.camiones.models.Camion;
+
+@Component
+public class CamionesService {
+    @Autowired
+    ICamionesDao camionesDao;
+
+
+    public List<Camion> getAll() {
+        List<Camion> listado = new ArrayList<>();
+        listado = this.camionesDao.findAll();
+        return listado;
+    }
+
+
+    public Optional<Camion> findById(Long id) {
+        Optional<Camion> find = this.camionesDao.findById(id);
+        if(find.isPresent()){
+            return find;
+        }
+        return Optional.empty();
+    }
+
+
+    public Camion save(Camion t) {
+        return this.camionesDao.save(t);
+    }
+
+
+    public void delete(Camion t) {
+        this.camionesDao.delete(t);
+    }
+
+
+
+}
